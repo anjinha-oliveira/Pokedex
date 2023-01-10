d1b3956b7e2190ff7d1e553669beae85ca267114
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-01-10 12:04:11 -0300

    Convertendo a lista de pokemons em HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,11 +23,11 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons) => {
-
+    const listItens = []
     for (let i = 0; i < pokemons.length; i++) {
         const pokemon = pokemons[i];
         console.log(convertPokemonToLi(pokemon))  
-        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        listItens.push(convertPokemonToLi(pokemon))
     }
-
+    console.log(listItens)
 })
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,4 +1,5 @@
 pokeApi = {}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
