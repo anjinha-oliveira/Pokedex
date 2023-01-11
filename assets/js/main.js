b2b2b0c14e054a23695946555f4a59f880939474
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-01-11 19:30:46 -0300

    fazendo a conversão do modelo da api para um novo modelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,19 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => (`<li class="type">${typeSlot.type.name}</li>`)
-)}
-
-
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#00${pokemon.order}</span>
+            <span class="number">#00${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}" />
+                <img src="${pokemon.photo}" alt="${pokemon.name}"/>
             </div>
         </li>
     `
