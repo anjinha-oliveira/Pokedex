6d9074a0921957edb0d87c8839f9b7a4da35399d
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-01-12 17:36:21 -0300

    Adicionando barra de rolagem e incrementando o offset e limit da página

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,30 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+let offset = 0
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#00${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHTML = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#00${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                    <div class="detail">
+                        <ol class="types">
+                            ${pokemon.types.map((type) =>`<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>        
+                        <img src="${pokemon.photo}" alt="${pokemon.name}"/>
+                    </div>
+            </li>
+        `).join('')
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}" alt="${pokemon.name}"/>
-            </div>
-        </li>
-    `
+        pokemonList.innerHTML += newHTML
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+loadMoreButton.addEventListener('click', () => {
+    offset += limit 
+    loadPokemonItens(offset, limit)
 })
